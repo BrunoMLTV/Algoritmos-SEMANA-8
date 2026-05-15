6e56d0330edb2e2e61dcf95f09612a837523c8d7
BrunoMLTV <bm665778@gmail.com>
2026-05-15 14:17:54 -0500

    Update DetectorCiclos.java

--- DetectorCiclos.java
+++ DetectorCiclos.java
@@ -9,22 +9,16 @@ package lab08;
  * @author LAB-USR-AREQUIPA
  */
 public class DetectorCiclos {
-
     public static boolean detectar(Nodo cabeza) {
-
         Nodo lento = cabeza;
         Nodo rapido = cabeza;
-
         while (rapido != null && rapido.siguiente != null) {
-
             lento = lento.siguiente;
             rapido = rapido.siguiente.siguiente;
-
             if (lento == rapido) {
                 return true;
             }
         }
-
         return false;
     }
 }
