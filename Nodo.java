fe0953d7f55d0b076c8bccf0d11bd850bbd9e5a3
BrunoMLTV <bm665778@gmail.com>
2026-05-15 14:21:45 -0500

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -9,10 +9,8 @@ package lab08;
  * @author LAB-USR-AREQUIPA
  */
 public class Nodo {
-
     int valor;
     Nodo siguiente;
-
     public Nodo(int valor) {
         this.valor = valor;
         this.siguiente = null;
