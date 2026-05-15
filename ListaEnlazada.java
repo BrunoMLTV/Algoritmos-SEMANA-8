de20d1876bf72b7cebd7f91f15c88a4bd9c604b1
BrunoMLTV <bm665778@gmail.com>
2026-05-15 14:19:26 -0500

    Update ListaEnlazada.java

--- ListaEnlazada.java
+++ ListaEnlazada.java
@@ -9,41 +9,29 @@ package lab08;
  * @author LAB-USR-AREQUIPA
  */
 public class ListaEnlazada {
-
     Nodo cabeza;
-
     public void insertar(int valor) {
-
         Nodo nuevo = new Nodo(valor);
-
         if (cabeza == null) {
             cabeza = nuevo;
             return;
         }
-
         Nodo actual = cabeza;
 
         while (actual.siguiente != null) {
             actual = actual.siguiente;
         }
-
         actual.siguiente = nuevo;
     }
-
     public void mostrar() {
-
         Nodo actual = cabeza;
-
         while (actual != null) {
             System.out.print(actual.valor);
-
             if (actual.siguiente != null) {
                 System.out.print(" -> ");
             }
-
             actual = actual.siguiente;
         }
-
         System.out.println();
     }
 }
